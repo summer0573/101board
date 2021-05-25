371a9e67a4a2333cb159c98187f117acf935c2e7
summer0573 <83990983+summer0573@users.noreply.github.com>
2021-05-25 10:05:26 +0900

    20칸에 있을시 가진 돈의 20프로를 기부

--- 보드게임.c
+++ 보드게임.c
@@ -42,6 +42,15 @@ void main(void)
 		//던진 주사위 값을 현재 위치에 적용
 		position += dice;
 		printf("현재 위치 : %d , 현재 금액 : %lld \n\n", position, money);
+		
+		if (position == 20)
+		{
+			printf("현재 위치 : %d , 현재 금액 : %lld \n\n", position, money);
+			printf("기부 천사! 재산의 20%를 사회에 기부합니다");
+			money = (long long)(money * 0.8);
+			printf("현재 위치 : %d, 현재 금액 : %lln \n\n", position, money);
+		}
+		
 
 
 

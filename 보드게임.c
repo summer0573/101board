00d74ff515a4e5281e94d0e38bb5249ad28446cd
summer0573 <83990983+summer0573@users.noreply.github.com>
2021-06-01 09:21:06 +0900

    머니 자료형을 롱롱에서 인트로 바꿈

--- 보드게임.c
+++ 보드게임.c
@@ -2,7 +2,7 @@
 * 보드개임
 *총 40칸
 * 플레이의 속성
-	1.돈 : 2.000,000,000(단위 : 백만) 시작
+	1.돈 : 20.000,000 시작
 	2. 현재위치 : 0부터 시작
 	3. 황금열쇠
 		원하는 곳 갈 수 있음
@@ -24,10 +24,10 @@ void main(void)
 	int dice;
 
 	//시작 2억
-	long long money = 2000000000;
+	int money = 20000000;
 	srand(time(0));
 
-	printf("현재 위치 : %d, 현재 금액 : %lld \n\n", position, money);
+	printf("현재 위치 : %d, 현재 금액 : %d \n\n", position, money);
 
 	//무한반복
 	while (1)
@@ -42,7 +42,7 @@ void main(void)
 
 		//던진 주사위 값을 현재 위치에 적용
 		position += dice;
-		printf("현재 위치 : %d , 현재 금액 : %lld \n\n", position, money);
+		printf("현재 위치 : %d , 현재 금액 : %d \n\n", position, money);
 
 		
 		switch (position)
@@ -56,25 +56,25 @@ void main(void)
 			int dice_money = rand() % 6 + 1;
 			money += 100000 * dice_money;
 			printf("주사위 %d이 나와서 %d원을 획득하였습니다. \n\n",dice_money, 100000 * dice_money);
-			printf("현재위치 : %d, 현재금액 : %lld \n\n", position, money);
+			printf("현재위치 : %d, 현재금액 : %d \n\n", position, money);
 			break;
 
 		case 20:
 			printf("[[기부 천사! 재산의 20%%를 사회에 기부합니다]]\n\n");
 			money = (long long)(money * 0.8);
-			printf("현재 위치 : %d, 현재 금액 : %lld \n\n", position, money);
+			printf("현재 위치 : %d, 현재 금액 : %d \n\n", position, money);
 			break;
 
 		case 30:
 			printf("[[던진 주사위의 수 만큼 더 나아갑니다.]]\n\n");
 			position += dice;
-			printf("현재 위치 : %d, 현재 금액 : %lld \n\n", position, money);
+			printf("현재 위치 : %d, 현재 금액 : %d \n\n", position, money);
 			break;
 
 		case 59:
 			printf("[[위치 59에 있을시 처음 지점으로 돌아갑니다.]]\n\n");
 			position = 0;
-			printf("현재 위치 : %d , 현재 금액 : %lld \n\n", position, money);
+			printf("현재 위치 : %d , 현재 금액 : %d \n\n", position, money);
 			break;
 		}
 

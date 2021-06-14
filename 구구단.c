5159964c413cf1bde191ba54a4d7aae73a99fa37
summer0573 <83990983+summer0573@users.noreply.github.com>
2021-06-14 14:50:56 +0900

    [문법 연습] 반복문 구구단

---
+++ 구구단.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(void)
+{
+	printf("2 x 1 = 2\n");
+	printf("2 x 2 = 4\n");
+	printf("2 x 3 = 6\n");
+	printf("2 x 4 = 8\n");
+	printf("2 x 5 = 10\n");
+	printf("2 x 6 = 12\n");
+	printf("2 x 7 = 14\n");
+	printf("2 x 8 = 16\n");
+	printf("2 x 9 = 18\n");
+	
+
+
+}

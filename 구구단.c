287034a01366b292f00a6773fd7f529ce5b5cc8c
summer0573 <83990983+summer0573@users.noreply.github.com>
2021-06-14 14:54:22 +0900

    [문법 연습] 구구단 %d

--- 구구단.c
+++ 구구단.c
@@ -2,15 +2,15 @@
 
 void main(void)
 {
-	printf("2 x 1 = 2\n");
-	printf("2 x 2 = 4\n");
-	printf("2 x 3 = 6\n");
-	printf("2 x 4 = 8\n");
-	printf("2 x 5 = 10\n");
-	printf("2 x 6 = 12\n");
-	printf("2 x 7 = 14\n");
-	printf("2 x 8 = 16\n");
-	printf("2 x 9 = 18\n");
+	printf("2 x 1 = %d\n", 2*1);
+	printf("2 x 2 = %d\n", 2*2);
+	printf("2 x 3 = %d\n", 2*3);
+	printf("2 x 4 = %d\n", 2*4);
+	printf("2 x 5 = %d\n", 2*5);
+	printf("2 x 6 = %d\n", 2*6);
+	printf("2 x 7 = %d\n", 2*7);
+	printf("2 x 8 = %d\n", 2*8);
+	printf("2 x 9 = %d\n", 2*9);
 	
 
 

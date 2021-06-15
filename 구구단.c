0109d9d73f43481ef273f7b7acd7e32f8f40665b
summer0573 <83990983+summer0573@users.noreply.github.com>
2021-06-15 09:10:45 +0900

    [문봅 공부] 구구단 2,3단 (이중 for문 사용)

--- 구구단.c
+++ 구구단.c
@@ -2,22 +2,13 @@
 
 void main(void)
 {
-	int dan = 2;
-	/*printf("%d x 1 = %d\n",dan , dan*1);
-	printf("%d x 2 = %d\n",dan , dan*2);
-	printf("%d x 3 = %d\n",dan , dan*3);
-	printf("%d x 4 = %d\n",dan , dan*4);
-	printf("%d x 5 = %d\n",dan , dan*5);
-	printf("%d x 6 = %d\n",dan , dan*6);
-	printf("%d x 7 = %d\n",dan , dan*7);
-	printf("%d x 8 = %d\n",dan , dan*8);
-	printf("%d x 9 = %d\n",dan , dan*9);*/
-
-	for (int a = 1; a <= 9; a++)
+	for (int dan = 2; dan <= 3; dan++)
 	{
-		printf("%d x %d = %d\n", dan, a, dan * a);
+		for (int a = 1; a <= 9; a++)
+		{
+			printf("%d x %d = %d\n", dan, a, dan * a);
+		}
 	}
-	
 
 
 }

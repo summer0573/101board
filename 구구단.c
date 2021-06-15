b7df928a540c6f59ff694628c0d06592f2142652
summer0573 <83990983+summer0573@users.noreply.github.com>
2021-06-15 10:03:58 +0900

    [문법 연습] 한줄짜리 코드는 중괄호 제거

--- 구구단.c
+++ 구구단.c
@@ -19,20 +19,15 @@ void main(void)
 	for (int dan = 2; dan <= 9; dan++)
 	{
 		if (dan == 4)
-		{
 			continue; //바깥쪽 for문에 대한 continue(4단만 skip)
 			//break; //바깥쪽 for문에 대한 break (단은 3까지만)
-		}
 
 		for (int a = 1; a <= 9; a++)
-		{
 			if (a == 5)
-			{
 				continue; //안쪽 for문에 대한 continue(수 5만 skip)
 				//break; //안쪽 for문에 대한 break (수는 4까지만)
-			}
-			printf("%d x %d = %d\n", dan, a, dan * a);
-		}
+
+		printf("%d x %d = %d\n", dan, a, dan * a);
 		printf("\n");
 	}
 

1c6552f3c806de00196197c88c54c76d8563372c
summer0573 <83990983+summer0573@users.noreply.github.com>
2021-06-15 09:37:16 +0900

    [문법 연습] 구구단 완성

--- 구구단.c
+++ 구구단.c
@@ -2,12 +2,27 @@
 
 void main(void)
 {
-	for (int dan = 2; dan <= 3; dan++)
+
+	/*while¹®
+	int dan = 2;
+	while (dan <= 3)
+	{
+		int i = 1;
+		while (i <= 9)
+		{
+			printf("%d x %d = %d\n", dan, i, dan * i);
+			i++;
+		}
+		dan++;
+	}*/
+
+	for (int dan = 2; dan <= 9; dan++)
 	{
 		for (int a = 1; a <= 9; a++)
 		{
 			printf("%d x %d = %d\n", dan, a, dan * a);
 		}
+		printf("\n");
 	}
 
 

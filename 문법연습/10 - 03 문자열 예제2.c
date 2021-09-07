133071242654a8a1827061c9dc4b56a5a6581bb0
summer0573 <83990983+summer0573@users.noreply.github.com>
2021-09-07 09:13:24 +0900

    반복문을 효율적으로 바꾸기

--- 문법연습/10 - 03 문자열 예제2.c
+++ 문법연습/10 - 03 문자열 예제2.c
@@ -2,8 +2,11 @@
 
 void main(void)
 {
-	char str[20] = "Hello World";
+	char str[] = "Hello World";
+	printf("%d\n", sizeof(str));
 
-	for (int i = 0; i < 12; i++)
+	//문자열의 갯수를 생각하지 않고 널문자 여부만을 확인
+	//문자의 끝에 도달 할 때 까지 반복 (추천 방법)
+	for (int i = 0; str[i] !='\0'; i++)
 		printf("%c", str[i]);
 }
